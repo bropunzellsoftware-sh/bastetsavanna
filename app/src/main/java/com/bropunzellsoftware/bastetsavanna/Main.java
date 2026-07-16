2e303d41c12b08ed48f58be7722dd66f61526752
Zeek <victor.araujo@vivensis.local>
2026-07-16 16:29:20 -0300

    Create initial Android project structure

---
+++ app/src/main/java/com/bropunzellsoftware/bastetsavanna/Main.java
@@ -0,0 +1,9 @@
+package com.bropunzellsoftware.bastetsavanna;
+
+public class Main{
+	
+	public static void start(){
+		System.out.println("Bastet Savanna started");
+	}
+
+}
---
+++ app/src/main/java/com/bropunzellsoftware/bastetsavanna/MainActivity.java
@@ -0,0 +1,12 @@
+package com.bropunzellsoftware.bastetsavanna;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void  onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+			Main.start();
+		}
+}
