a49c78b9d6db78f36d8f31988a55594af3c5b599
bropunzellsoftware-sh <bropunzellsoftware@gmail.com>
2026-08-01 20:58:58 -0300

    Atualizar o Umidade.java

--- app/src/main/java/com/bropunzellsoftware/bastetsavanna/Umidade.java
+++ app/src/main/java/com/bropunzellsoftware/bastetsavanna/Umidade.java
@@ -1,4 +1,4 @@
-package com.bropunzellsoftware.bastetsavanna.acesso;
+/* package com.bropunzellsoftware.bastetsavanna.acesso;
 
 import android.content.Context;
 import android.hardware.Sensor;
@@ -14,10 +14,7 @@ public final class Umidade {
     // HARDWARE
     // ==========================
 
-    /**
-     * Verifica se o aparelho possui
-     * sensor de umidade relativa.
-     */
+    
     public static boolean possui(Context context) {
 
         SensorManager manager =
@@ -44,9 +41,7 @@ public final class Umidade {
     // PERMISSÕES
     // ==========================
 
-    /**
-     * Sensor de umidade não necessita permissão.
-     */
+    
     public static boolean temPermissao(Context context) {
 
         return true;
@@ -72,13 +67,11 @@ public final class Umidade {
     // ESTADO
     // ==========================
 
-    /**
-     * Sensor de umidade disponível.
-     */
+    
     public static boolean disponivel(Context context) {
 
         return possui(context);
 
     }
 
-}
+} */
